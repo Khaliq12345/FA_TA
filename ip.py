c9e84aa34c36bf332900032d4f5a962ba4efcb6f
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2022-12-07 09:14:16 +0100

    Update ip.py

--- ip.py
+++ ip.py
@@ -22,7 +22,8 @@ if button:
 
 
         page.locator(f'div.tool-button:text("{symbol}")').click()
-
+        
+        sleep(2)
         soup = BeautifulSoup(page.content(), 'lxml')
 
         buy = soup.select_one('#main-container > div.col-md-8 > div.tool-content.full-width.sharable-content > div.tools > div.row > div.tool-content.col-sm-8.sentiment-ratios > div > div.cur-rat-pairs > div.cur-rat-cur-pair.header.active > div.line.cur-rat-broker.broker-average > div > div > div.ratio > div.ratio-bar-left').text.strip()
