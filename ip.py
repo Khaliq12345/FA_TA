f81e1ea25751213f0f1f81c04fe6549521bf4983
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2022-12-07 03:23:03 +0100

    Update ip.py

--- ip.py
+++ ip.py
@@ -25,15 +25,11 @@ if symbol:
 
         buy = soup.select_one('#main-container > div.col-md-8 > div.tool-content.full-width.sharable-content > div.tools > div.row > div.tool-content.col-sm-8.sentiment-ratios > div > div.cur-rat-pairs > div.cur-rat-cur-pair.header.active > div.line.cur-rat-broker.broker-average > div > div > div.ratio > div.ratio-bar-left').text.strip()
         sell = soup.select_one('#main-container > div.col-md-8 > div.tool-content.full-width.sharable-content > div.tools > div.row > div.tool-content.col-sm-8.sentiment-ratios > div > div.cur-rat-pairs > div.cur-rat-cur-pair.header.active > div.line.cur-rat-broker.broker-average > div > div > div.ratio > div.ratio-bar-right').text.strip()
+        buy = buy.replace('%', '')
+        sell = buy.replace('%', '')
         
-        print(f'Buy: {buy}')
-        print(f'Sell: {sell}')
-
         # other actions...
         browser.close()
-
-buy = buy.replace('%', '')
-sell = buy.replace('%', '')
 
 col1, col2 = st.columns(2)
 
