e95d40f087582bb1eef2da6267a78b1ac5dbf051
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2022-12-07 15:51:48 +0100

    Update ip.py

--- ip.py
+++ ip.py
@@ -17,40 +17,41 @@ symbol1 = column1.selectbox('Symbol to analyse the sentiments', (
 ))
 button = column1.button('Analyse')
 
-with sync_playwright() as playwright:
-    ua = get_random_user_agent()
-    chromium = playwright.chromium # or "firefox" or "webkit".
-    browser = chromium.launch()
-    page = browser.new_page(extra_http_headers={
-        'User-Agent': ua
-    })
-    page.goto("https://fxssi.com/tools/current-ratio?filter=AUDJPY", timeout=100000)
+if button:
+    with sync_playwright() as playwright:
+        ua = get_random_user_agent()
+        chromium = playwright.chromium # or "firefox" or "webkit".
+        browser = chromium.launch()
+        page = browser.new_page(extra_http_headers={
+            'User-Agent': ua
+        })
+        page.goto("https://fxssi.com/tools/current-ratio?filter=AUDJPY", timeout=100000)
 
 
-    page.locator(f'div.tool-button:text("{symbol1}")').click()
-    sleep(2)
+        page.locator(f'div.tool-button:text("{symbol1}")').click()
+        sleep(2)
 
-    soup = BeautifulSoup(page.content(), 'lxml')
+        soup = BeautifulSoup(page.content(), 'lxml')
 
-    buy = soup.select_one('#main-container > div.col-md-8 > div.tool-content.full-width.sharable-content > div.tools > div.row > div.tool-content.col-sm-8.sentiment-ratios > div > div.cur-rat-pairs > div.cur-rat-cur-pair.header.active > div.line.cur-rat-broker.broker-average > div > div > div.ratio > div.ratio-bar-left').text.strip()
-    sell = soup.select_one('#main-container > div.col-md-8 > div.tool-content.full-width.sharable-content > div.tools > div.row > div.tool-content.col-sm-8.sentiment-ratios > div > div.cur-rat-pairs > div.cur-rat-cur-pair.header.active > div.line.cur-rat-broker.broker-average > div > div > div.ratio > div.ratio-bar-right').text.strip()
-    buy = buy.replace('%', '')
-    sell = sell.replace('%', '')
+        buy = soup.select_one('#main-container > div.col-md-8 > div.tool-content.full-width.sharable-content > div.tools > div.row > div.tool-content.col-sm-8.sentiment-ratios > div > div.cur-rat-pairs > div.cur-rat-cur-pair.header.active > div.line.cur-rat-broker.broker-average > div > div > div.ratio > div.ratio-bar-left').text.strip()
+        sell = soup.select_one('#main-container > div.col-md-8 > div.tool-content.full-width.sharable-content > div.tools > div.row > div.tool-content.col-sm-8.sentiment-ratios > div > div.cur-rat-pairs > div.cur-rat-cur-pair.header.active > div.line.cur-rat-broker.broker-average > div > div > div.ratio > div.ratio-bar-right').text.strip()
+        buy = buy.replace('%', '')
+        sell = sell.replace('%', '')
 
-    # other actions...
-    browser.close()
+        # other actions...
+        browser.close()
 
-    col1, col2 = st.columns(2)
+        col1, col2 = st.columns(2)
 
-    if buy > sell:
-        buy2 = buy
-        sell2 = "-" + sell
-    else:
-        buy2 = '-' + buy
-        sell2 = sell
+        if buy > sell:
+            buy2 = buy
+            sell2 = "-" + sell
+        else:
+            buy2 = '-' + buy
+            sell2 = sell
 
-    column1.metric('Buy', value = float(buy), delta = buy2)
-    column1.metric('Sell', value = float(sell), delta = sell2)
+        column1.metric('Buy', value = float(buy), delta = buy2)
+        column1.metric('Sell', value = float(sell), delta = sell2)
         
 symbol2 = column2.selectbox('Symbol to analyse', (
     'EURUSD', 'AUDUSD', 'AUDJPY', 'EURAUD', 'EURJPY', 
